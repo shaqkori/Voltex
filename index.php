5d3d5ad6e905ba14c3579779fa151c57ebd65434
Shaqkori <mahoney.shaqkori@gmail.com>
2023-12-05 10:12:44 +0000

    website layout

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Simple PHP Website</title>
+</head>
+<body>
+
+    <header>
+        <h1>Welcome to My PHP Website</h1>
+    </header>
+
+    <main>
+        <section>
+            <h2>About Us</h2>
+            <p>This is a simple PHP website with CSS styling.</p>
+        </section>
+
+        <section>
+            <h2>Contact</h2>
+            <p>You can reach us at contact@example.com.</p>
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; 2023 Simple PHP Website</p>
+    </footer>
+
+</body>
+</html>
